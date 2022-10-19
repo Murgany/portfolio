37e0a41a3c314de1d0b199df8c71b3fb9d2fbe92
Murgany <76653393+Murgany@users.noreply.github.com>
2022-10-19 20:59:13 +0200

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -92,44 +92,52 @@ const Projects = () => {
                     <span className="responsive-text"><ReadMore>{project.technology}</ReadMore></span>
                   </div>
 
-                  <div className="justify-content-between col-12 mb-2 pt-4">
-                    <Button
-                      href={project.github}
-                      target="_blank"
-                      style={{ borderRadius: "50%" }}
-                      variant="light"
-                      className="btn-sm p-1 icons git-link"
-                      rel="noreferrer"
-                      id="icons"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="bi bi-code-slash p-1 contact-icons"
-                        viewBox="0 0 16 16"
+ <div className="justify-content-between col-12 mb-2 pt-4 text-cente">
+                    {project.github ? (
+                      <Button
+                        href={project.github}
+                        style={{ borderRadius: "50%" }}
+                        variant="light"
+                        className="btn-sm p-1 icons git-link"
+                        rel="noreferrer"
+                        id="icons"
                       >
-                        <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z" />
-                      </svg>
-                    </Button>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="bi bi-code-slash p-1 contact-icons"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z" />
+                        </svg>
+                      </Button>
+                    ) : (
+                      <></>
+                    )}
 
-                    <Button
-                      href={project.link}
-                      target="_blank"
-                      rel="noreferrer"
-                      style={{ borderRadius: "50%" }}
-                      variant="light"
-                      className="btn-sm p-1 icons site-link"
-                      id="icons"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="bi bi-envelope p-1 contact-icons"
-                        viewBox="0 0 16 16"
+                    {project.link ? (
+                      <Button
+                        href={project.link}
+                        target="_blank"
+                        rel="noreferrer"
+                        style={{ borderRadius: "50%" }}
+                        variant="light"
+                        className="btn-sm p-1 icons site-link"
+                        id="icons"
                       >
-                        <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
-                        <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z" />
-                      </svg>
-                    </Button>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="bi bi-envelope p-1 contact-icons"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
+                          <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z" />
+                        </svg>
+                      </Button>
+                    ) : (
+                      <></>
+                    )}
                   </div>
+
                 </Card.Body>
               </div>
             </Card>
