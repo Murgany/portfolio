6730abab4fb2788144f4e16183768c4299416f16
Murgany <76653393+Murgany@users.noreply.github.com>
2022-12-10 03:12:39 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export const projects = [
   },
   {
     title: "MEDEC-GULF",
-    subtitle: "Merck group digital engagement center website: Designed by a friend, I built the backend",
+    subtitle: "Merck group digital engagement center: Designed by a friend, I built the backend & a bit of the frontend",
     description: "A merck group digital engagement center website used for medical content in form of video episodes, accompanied by optional, dynamically timed survey pop-ups in every video episode. An optional user registration system is available if the admin wants survey participants to be authenticated users only. User details are saved in the data base and are exportable to Excel-compatible CSV file. The admin dashboard features a survey results view and a button to export the results to Excel-compatible CSV file. Simple looking on the client side, with the work-load on the backend side and the heavily Customized Django admin dashboard",
     technology: "Django, HTML, CSS, Bootstrap, Ajax, JQuery.",
     image: "./medecgulf.png",
